8dd38a13a43ef10c8b1f4c7f1bc0e342f0a36cb7
cuerposaco <ivan.rtve@gmail.com>
2020-08-30 22:14:52 +0200

    feature: remove CMS host

--- src/templates/page.js
+++ src/templates/page.js
@@ -13,45 +13,45 @@ import { MetaData } from '../components/common/meta'
 *
 */
 const Page = ({ data, location }) => {
-    const page = data.ghostPage
+  const page = data.ghostPage
 
-    return (
-        <>
-            <MetaData
-                data={data}
-                location={location}
-                type="website"
+  return (
+    <>
+      <MetaData
+        data={data}
+        location={location}
+        type="website"
+      />
+      <Helmet>
+        <style type="text/css">{`${page.codeinjection_styles}`}</style>
+      </Helmet>
+      <Layout>
+        <div className="container">
+          <article className="content">
+            <h1 className="content-title">{page.title}</h1>
+
+            {/* The main page content */}
+            <section
+              className="content-body load-external-scripts"
+              dangerouslySetInnerHTML={{ __html: page.html.replace('http://ivanmejias.me:8080', '') }}
             />
-            <Helmet>
-                <style type="text/css">{`${page.codeinjection_styles}`}</style>
-            </Helmet>
-            <Layout>
-                <div className="container">
-                    <article className="content">
-                        <h1 className="content-title">{page.title}</h1>
-
-                        {/* The main page content */}
-                        <section
-                            className="content-body load-external-scripts"
-                            dangerouslySetInnerHTML={{ __html: page.html }}
-                        />
-                    </article>
-                </div>
-            </Layout>
-        </>
-    )
+          </article>
+        </div>
+      </Layout>
+    </>
+  )
 }
 
 Page.propTypes = {
-    data: PropTypes.shape({
-        ghostPage: PropTypes.shape({
-            codeinjection_styles: PropTypes.object,
-            title: PropTypes.string.isRequired,
-            html: PropTypes.string.isRequired,
-            feature_image: PropTypes.string,
-        }).isRequired,
+  data: PropTypes.shape({
+    ghostPage: PropTypes.shape({
+      codeinjection_styles: PropTypes.object,
+      title: PropTypes.string.isRequired,
+      html: PropTypes.string.isRequired,
+      feature_image: PropTypes.string,
     }).isRequired,
-    location: PropTypes.object.isRequired,
+  }).isRequired,
+  location: PropTypes.object.isRequired,
 }
 
 export default Page
