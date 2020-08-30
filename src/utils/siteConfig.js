611ba6547c346d5be9892d2103632f9d0f2cfbf6
cuerposaco <ivan.rtve@gmail.com>
2020-08-30 17:28:10 +0200

    update site settings and logo

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,8 +3,8 @@ module.exports = {
 
   postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-  siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-  siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+  siteTitleMeta: `Los asesinos del Cómic`, // This allows an alternative site title for meta data for pages.
+  siteDescriptionMeta: `El lugar donde los las historias se convierten en viñetas`, // This allows an alternative site description for meta data for pages.
 
   shareImageWidth: 1000, // Change to the width of your default share image
   shareImageHeight: 523, // Change to the height of your default share image
